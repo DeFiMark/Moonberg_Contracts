f6f70a195ca68e3757d62e31a248fda2f0f656c1
DeFiMark <mark@dappd.net>
2024-06-04 18:11:24 -0400

    first commit

---
+++ MoonbergToken.sol
@@ -0,0 +1,336 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+/**
+ * @title Owner
+ * @dev Set & change owner
+ */
+contract Ownable {
+
+    address private owner;
+    
+    // event for EVM logging
+    event OwnerSet(address indexed oldOwner, address indexed newOwner);
+    
+    // modifier to check if caller is owner
+    modifier onlyOwner() {
+        // If the first argument of 'require' evaluates to 'false', execution terminates and all
+        // changes to the state and to Ether balances are reverted.
+        // This used to consume all gas in old EVM versions, but not anymore.
+        // It is often a good idea to use 'require' to check if functions are called correctly.
+        // As a second argument, you can also provide an explanation about what went wrong.
+        require(msg.sender == owner, "Caller is not owner");
+        _;
+    }
+    
+    /**
+     * @dev Set contract deployer as owner
+     */
+    constructor() {
+        owner = msg.sender; // 'msg.sender' is sender of current call, contract deployer for a constructor
+        emit OwnerSet(address(0), owner);
+    }
+
+    /**
+     * @dev Change owner
+     * @param newOwner address of new owner
+     */
+    function changeOwner(address newOwner) public onlyOwner {
+        emit OwnerSet(owner, newOwner);
+        owner = newOwner;
+    }
+
+    /**
+     * @dev Return owner address 
+     * @return address of owner
+     */
+    function getOwner() external view returns (address) {
+        return owner;
+    }
+}
+
+interface IERC20 {
+
+    function totalSupply() external view returns (uint256);
+    
+    function symbol() external view returns(string memory);
+    
+    function name() external view returns(string memory);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+    
+    /**
+     * @dev Returns the number of decimal places
+     */
+    function decimals() external view returns (uint8);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+
+// helper methods for interacting with ERC20 tokens and sending ETH that do not consistently return true/false
+library TransferHelper {
+    function safeApprove(
+        address token,
+        address to,
+        uint256 value
+    ) internal {
+        // bytes4(keccak256(bytes('approve(address,uint256)')));
+        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0x095ea7b3, to, value));
+        require(
+            success && (data.length == 0 || abi.decode(data, (bool))),
+            'TransferHelper::safeApprove: approve failed'
+        );
+    }
+
+    function safeTransfer(
+        address token,
+        address to,
+        uint256 value
+    ) internal {
+        // bytes4(keccak256(bytes('transfer(address,uint256)')));
+        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0xa9059cbb, to, value));
+        require(
+            success && (data.length == 0 || abi.decode(data, (bool))),
+            'TransferHelper::safeTransfer: transfer failed'
+        );
+    }
+
+    function safeTransferFrom(
+        address token,
+        address from,
+        address to,
+        uint256 value
+    ) internal {
+        // bytes4(keccak256(bytes('transferFrom(address,address,uint256)')));
+        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0x23b872dd, from, to, value));
+        require(
+            success && (data.length == 0 || abi.decode(data, (bool))),
+            'TransferHelper::transferFrom: transferFrom failed'
+        );
+    }
+
+    function safeTransferETH(address to, uint256 value) internal {
+        (bool success, ) = to.call{value: value}(new bytes(0));
+        require(success, 'TransferHelper::safeTransferETH: ETH transfer failed');
+    }
+}
+
+/**
+    Moonberg Token
+ */
+contract Moonberg is IERC20, Ownable {
+
+    // total supply
+    uint256 private _totalSupply = 1_000_000_000 * 10**18;
+
+    // token data
+    string private constant _name = 'MOONBERG';
+    string private constant _symbol = 'OO';
+    uint8  private constant _decimals = 18;
+
+    // balances
+    mapping (address => uint256) private _balances;
+    mapping (address => mapping (address => uint256)) private _allowances;
+    
+    constructor(address _initialOwner) {
+
+        // initial supply allocation
+        _balances[_initialOwner] = _totalSupply;
+        emit Transfer(address(0), _initialOwner, _totalSupply);
+
+        // change owner to new owner
+        this.changeOwner(_initialOwner);
+    }
+
+    /////////////////////////////////
+    /////    ERC20 FUNCTIONS    /////
+    /////////////////////////////////
+
+    function totalSupply() external view override returns (uint256) { return _totalSupply; }
+    function balanceOf(address account) public view override returns (uint256) { return _balances[account]; }
+    function allowance(address holder, address spender) external view override returns (uint256) { return _allowances[holder][spender]; }
+    
+    function name() public pure override returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public pure override returns (string memory) {
+        return _symbol;
+    }
+
+    function decimals() public pure override returns (uint8) {
+        return _decimals;
+    }
+
+    function approve(address spender, uint256 amount) public override returns (bool) {
+        _allowances[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    /** Transfer Function */
+    function transfer(address recipient, uint256 amount) external override returns (bool) {
+        return _transfer(msg.sender, recipient, amount);
+    }
+
+    /** Transfer Function */
+    function transferFrom(address sender, address recipient, uint256 amount) external override returns (bool) {
+        require(
+            _allowances[sender][msg.sender] >= amount,
+            'Insufficient Allowance'
+        );
+        _allowances[sender][msg.sender] -= amount;
+        return _transfer(sender, recipient, amount);
+    }
+
+
+    /////////////////////////////////
+    /////   PUBLIC FUNCTIONS    /////
+    /////////////////////////////////
+
+
+    function burn(uint256 amount) external returns (bool) {
+        return _burn(msg.sender, amount);
+    }
+
+    function burnFrom(address account, uint256 amount) external returns (bool) {
+        require(
+            _allowances[account][msg.sender] >= amount,
+            'Insufficient Allowance'
+        );
+        _allowances[account][msg.sender] -= amount;
+        return _burn(account, amount);
+    }
+
+    receive() external payable {}
+
+    /////////////////////////////////
+    /////    OWNER FUNCTIONS    /////
+    /////////////////////////////////
+
+    function withdraw(address token, address to, uint256 amount) external onlyOwner {
+        require(token != address(0), 'Zero');
+        TransferHelper.safeTransfer(token, to, amount);
+    }
+
+    function withdrawETH(address to, uint256 amount) external onlyOwner {
+        TransferHelper.safeTransferETH(to, amount);
+    }
+    
+    //////////////////////////////////
+    /////   INTERNAL FUNCTIONS   /////
+    //////////////////////////////////
+
+    /** Internal Transfer */
+    function _transfer(address sender, address recipient, uint256 amount) internal returns (bool) {
+        require(
+            recipient != address(0),
+            'Zero Recipient'
+        );
+        require(
+            amount > 0,
+            'Zero Amount'
+        );
+        require(
+            amount <= _balances[sender],
+            'Insufficient Balance'
+        );
+        
+        // decrement sender balance
+        _balances[sender] -= amount;
+        unchecked {
+            // add amount to recipient balance
+            _balances[recipient] += amount;
+        }
+
+        // emit Transfer event for block explorers
+        emit Transfer(sender, recipient, amount);
+
+        // return success
+        return true;
+    }
+
+    function _burn(address account, uint256 amount) internal returns (bool) {
+        require(
+            account != address(0),
+            'Zero Address'
+        );
+        require(
+            amount > 0,
+            'Zero Amount'
+        );
+        require(
+            amount <= _balances[account],
+            'Insufficient Balance'
+        );
+
+        // delete from balance and supply
+        _balances[account] -= amount;
+        _totalSupply -= amount;
+
+        // emit transfer
+        emit Transfer(account, address(0), amount);
+        return true;
+    }
+}
---
+++ VestingSchedule.sol
@@ -0,0 +1,224 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+/**
+ * @title Owner
+ * @dev Set & change owner
+ */
+contract Ownable {
+
+    address private owner;
+    
+    // event for EVM logging
+    event OwnerSet(address indexed oldOwner, address indexed newOwner);
+    
+    // modifier to check if caller is owner
+    modifier onlyOwner() {
+        // If the first argument of 'require' evaluates to 'false', execution terminates and all
+        // changes to the state and to Ether balances are reverted.
+        // This used to consume all gas in old EVM versions, but not anymore.
+        // It is often a good idea to use 'require' to check if functions are called correctly.
+        // As a second argument, you can also provide an explanation about what went wrong.
+        require(msg.sender == owner, "Caller is not owner");
+        _;
+    }
+    
+    /**
+     * @dev Set contract deployer as owner
+     */
+    constructor() {
+        owner = msg.sender; // 'msg.sender' is sender of current call, contract deployer for a constructor
+        emit OwnerSet(address(0), owner);
+    }
+
+    /**
+     * @dev Change owner
+     * @param newOwner address of new owner
+     */
+    function changeOwner(address newOwner) public onlyOwner {
+        emit OwnerSet(owner, newOwner);
+        owner = newOwner;
+    }
+
+    /**
+     * @dev Return owner address 
+     * @return address of owner
+     */
+    function getOwner() external view returns (address) {
+        return owner;
+    }
+}
+
+interface IERC20 {
+
+    function totalSupply() external view returns (uint256);
+    
+    function symbol() external view returns(string memory);
+    
+    function name() external view returns(string memory);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+    
+    /**
+     * @dev Returns the number of decimal places
+     */
+    function decimals() external view returns (uint8);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+
+contract MoonbergVesting is Ownable {
+
+    /** Token To Be Vested */
+    IERC20 public immutable token;
+
+    /** 
+        Vesting Structure     
+    */
+    struct Vesting {
+        uint256 totalVest;       // total amount of tokens to be vested
+        uint256 amount;          // amount of tokens left to be claimed or vested
+        uint256 startTime;       // start time of the vesting
+        uint256 duration;        // duration in seconds of vesting
+        uint256 lastClaim;       // last time a claim was made by the user
+        uint256 tokensPerSecond; // tokens to be vested per second (amount / duration)
+    }
+    mapping ( address => Vesting ) public vesting;
+
+    constructor(
+        address token_
+    ) {
+        token = IERC20(token_);
+    }
+
+    function addVestingInfo(
+        address user,
+        uint256 amount,
+        uint256 duration
+    ) external onlyOwner {
+        require(amount > 0, "Amount must be greater than 0");
+        require(duration > 0, "Duration must be greater than 0");
+        require(vesting[user].amount == 0, "Vesting info already exists");
+        vesting[user] = Vesting({
+            totalVest: amount,
+            amount: amount,
+            startTime: block.timestamp,
+            duration: duration,
+            lastClaim: block.timestamp,
+            tokensPerSecond: amount / duration
+        });
+    }
+
+    function addBatchVestingInfo(
+        address[] calldata users,
+        uint256[] calldata amounts,
+        uint256[] calldata durations
+    ) external onlyOwner {
+        uint len = users.length;
+        require(len == amounts.length && len == durations.length, "Array length mismatch");
+        for (uint256 i = 0; i < len;) {
+            require(amounts[i] > 0, "Amount must be greater than 0");
+            require(durations[i] > 0, "Duration must be greater than 0");
+            require(vesting[users[i]].amount == 0, "Vesting info already exists");
+            vesting[users[i]] = Vesting({
+                totalVest: amounts[i],
+                amount: amounts[i],
+                startTime: block.timestamp,
+                duration: durations[i],
+                lastClaim: block.timestamp,
+                tokensPerSecond: amounts[i] / durations[i]
+            });
+        }
+    }
+
+    function claim() external {
+        require(vesting[msg.sender].amount > 0, "No vesting info found");
+        uint256 pending = pendingRewards(msg.sender);
+        require(pending > 0, "No rewards to claim");
+        vesting[msg.sender].lastClaim = block.timestamp;
+        vesting[msg.sender].amount -= pending;
+        token.transfer(msg.sender, pending);
+    }
+
+    function pendingRewards(address user) public view returns (uint256) {
+        Vesting memory v = vesting[user];
+        if (v.amount == 0) {
+            return 0;
+        }
+        uint256 timeElapsed = timeSinceClaim(user);
+        if (timeElapsed == 0) {
+            return 0;
+        }
+        uint256 tokensToClaim = timeElapsed * v.tokensPerSecond;
+        if (tokensToClaim > v.amount) {
+            tokensToClaim = v.amount;
+        }
+        return tokensToClaim;
+    }
+
+    function timeSinceClaim(address user) public view returns (uint256) {
+        return block.timestamp > vesting[user].lastClaim ? block.timestamp - vesting[user].lastClaim : 0;
+    }
+
+}
