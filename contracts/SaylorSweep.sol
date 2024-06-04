ab27fb4b3b7cfceb8d9e4401ccfb9a8531569284
DeFiMark <mark@dappd.net>
2024-06-04 18:28:15 -0400

    Added cross chain, separated files into contracts/ folder

---
+++ contracts/SaylorSweep.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import "../lib/Ownable.sol";
+import "../lib/IERC20.sol";
+
+contract SaylorSweep is Ownable {
+
+    address[] public backingAssets;
+
+
+
+
+}
